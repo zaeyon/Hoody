87d51073f580f5dc83421dec87c456ec05fddf51
Jaeyeon2 <wodus5677@gmail.com>
2020-07-12 13:54:14 +0900

    implement thumbnail marker in location feed map

--- src/Components/Container/LocationFeedMapScreen/index.tsx
+++ src/Components/Container/LocationFeedMapScreen/index.tsx
@@ -5,7 +5,7 @@ import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
 
 const Container = Styled.SafeAreaView`
@@ -50,12 +50,54 @@ const MapContainer = Styled.View`
  background-color: #ffffff;
 `;
 
+const MarkerContainer = Styled.View`
+ background-color: #c3c3c3;
+`;
+
+const MarkerThumbnailImage = Styled.Image`
+ width: 48px;
+ height: 48px;
+ border-radius: 50px;
+`;
+
+const MarkerThumbnailContainer = Styled.View`
+ flex:1;
+ justify-content: center;
+ align-items: center;
+ padding: 3.5px;
+`;
+
 interface Props {
     navigation: any,
     route: any,
 }
 
+const TEST_LOCATION_FEED_DATA = [
+    {
+        index: 1,
+        imageUri: 'https://mp-seoul-image-production-s3.mangoplate.com/1658798_1590602456196417.jpg',
+        location: {
+            latitude: 37.565633,
+            longitude: 126.991251,
+        }
+    },
+    {
+        index: 1,
+        imageUri: 'https://t1.daumcdn.net/cfile/tistory/99A83E3F5BC97A5E19',
+        location: {
+            latitude: 37.566313,
+            longitude: 126.987941,
+        }
+    }
+]
+
 const LocationFeedMapScreen = ({navigation, route}: Props) => {
+    const [locationFeedData, setLocationFeedData] = useState<Array<object>>();
+
+    useEffect(() => {
+       setLocationFeedData(TEST_LOCATION_FEED_DATA); 
+    }, [])
+
     return (
         <Container>
             <HeaderBar>
@@ -78,7 +120,22 @@ const LocationFeedMapScreen = ({navigation, route}: Props) => {
                 longitude: 126.998215,
                 latitudeDelta: 0.0022,
                 longitudeDelta: 0.0421,
-            }}/>
+            }}>
+                {locationFeedData?.map(feed => (
+                    <Marker
+                    style={{
+                        justifyContent:'center',
+                        alignItems:'center',
+                    }}
+                    coordinate={feed.location}
+                    image={require('~/Assets/Images/ic_circleMarker_middle.png')}>
+                        <MarkerThumbnailContainer>
+                            <MarkerThumbnailImage
+                            source={{uri:feed.imageUri}}/>
+                        </MarkerThumbnailContainer>
+                    </Marker>
+                ))}
+            </MapView>
         </Container>
     )
 }

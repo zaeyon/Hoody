da973dee23203c453d697bf555c08169dc99a910
Jaeyeon2 <wodus5677@gmail.com>
2020-09-09 12:22:34 +0900

    소셜로그인시 자동로그인 안되는 오류 해결

--- src/Route/Auth/POSTSocial.js
+++ src/Route/Auth/POSTSocial.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
+import {setAutoLoginUser} from '~/AsyncStorage/User';
 
 const baseUrl = 'http://hoody-api-test-server-alb-349396782.ap-northeast-2.elb.amazonaws.com';
 
 
-const POSTSocial = (socialId, email ,provider) => {
+const POSTSocial = (socialId, email ,provider, fcmToken) => {
     const url = baseUrl + '/auth/social';
-
     console.log("POSTSocial socialId", socialId);
     console.log("POSTSocial email", email);
     console.log("POSTSocial provider", provider);
+    console.log("POSTSocial fcmToken")
 
     let form = new FormData();
     form.append('socialId', socialId);
     form.append('email', email);
     form.append('provider', provider);
+
     return new Promise(function (resolve, reject) {
       axios
         .post(url, form, {
@@ -25,6 +27,8 @@ const POSTSocial = (socialId, email ,provider) => {
         })
         .then(function (response) {
           resolve(response.data);
+          console.log("소셜 로그인 성공 response", response);
+          setAutoLoginUser(email, response.data.user.nickname, response.data.user.id, response.data.sessionId, "login");
         })
         .catch(function (error) {
           reject(error.response);
